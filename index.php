70af9d471285b8b82be59dcf1a72fe7242589d57
Sergei_Pro <sergeipro@list.ru>
2018-07-12 18:03:04 +0300

    php  update test

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?phpinfo(); ?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello World!</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+<p>
+<?php echo "It`s working!!"; ?>
+</body>
+</html>
