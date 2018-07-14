10a24be38b2a79c4124afe4300b472700a2dd68b
SergiO <test@myip.com>
2018-07-14 16:57:09 +0300

    Add test jquery

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="utf-8">
   <title>Hello World!</title>
+  <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+  
+  $(document).ready(function(){
+    $("#msgid").html("Set text)))");
+  });
+  </script>
 </head>
 <body>
     <h1>Hello World!</h1>
@@ -11,6 +17,8 @@
 <?php echo "It`s working!!"; ?>
 
 </p>
+<div id="msgid">
+</div>
 
 </body>
 </html>
