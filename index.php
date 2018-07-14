d7a80ef26404c88679cdf2594274744d3520bcc8
Sergei_Pro <sergeipro@list.ru>
2018-07-15 01:34:22 +0300

    New version

--- index.php
+++ index.php
@@ -1,77 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Hello World!</title>
-  <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
-  <script type="text/javascript">
-     $(document).ready(function(){
-       $(".btn-slide").click(function(){ $("#panel").slideToggle("show");
-         $(this).toggleClass("active"); return false;
-       });
-     });
-  </script>
-  <style type="text/css">
-  
-body {
-	margin: 0 auto;
-	padding: 0;
-	width: 570px;
-	font: 75%/120% Arial, Helvetica, sans-serif;
-}
-a:focus {
-	outline: none;
-}
-#panel {
-	background: #754c24;
-	height: 200px;
-	display: none;
-}
-.slide {
-	margin: 0;
-	padding: 0;
-	border-top: solid 4px #422410;
-	background-color: red;
-}
-.btn-slide {
-	text-align: center;
-	width: 144px;
-	height: 31px;
-	padding: 10px 10px 0 0;
-	margin: 0 auto;
-	display: block;
-	font: bold 120%/100% Arial, Helvetica, sans-serif;
-	color: #fff;
-	text-decoration: none;
-}
-.active {
-	background-position: right 12px;
-}
+<?php
+  require_once("header.php");
+?>
 
-  </style>
-</head>
-<body>
-  <div id="panel" style="display:none;"></div>
-  <p class="slide">
-    <a class="btn-slide" href="#">Login</a>
+<div id="content">
+  <h2>Контент сайта</h2>
+  <p>
+  Однажды шторм закончится, и ты не вспомнишь, как его пережил. Ты даже не будешь уверен в том, закончился ли он на самом деле.
+
+  Но одна вещь бесспорна: когда ты выйдешь из шторма, ты никогда снова не станешь тем человеком, который вошёл в него. Потому что в этом и был весь его смысл.
+  © Харуки Мураками
   </p>
-  <script type="text/javascript">
-    $(document).ready(function(){
-      $("#msgid").html("Set text)))");
-    });
-  </script>
-
-    <h1>Hello World!</h1>
-<p>
-
-<?php echo "It`s working!!"; ?>
-
-</p>
-<div id="msgid">
 </div>
 
-<div>
-</div>
-
-</body>
-</html>
+<?php
+  require_once("footer.php");
+?>
