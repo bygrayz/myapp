187ad857bf83cdc7a2c85f179d10a98313380d58
Sergei_Pro <sergeipro@list.ru>
2018-07-23 23:01:00 +0300

    Add check email and pass len

--- header.php
+++ header.php
@@ -8,7 +8,42 @@
     <title>Territory of GOOD Thought</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-  <head>
+    <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function(){
+        "use strict";
+        var pattern = /^[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i;
+        var mail = $('input[name=email]');
+        mail.blur(function(){
+         if(mail.val() != ''){
+           if(mail.val().search(pattern) ==0){
+             $('#valid_email_message').text('');
+             $('input[type=submit]').attr('disable',false);
+           }else{
+             $('#valid_email_message').text('ERROR email');
+             $('input[type=submit]').attr('disable',true);
+           }
+         }else{
+           $('#valid_email_message').text('Set you email');
+         }
+       });
+       var password = $('input[input[name=password]');
+       password.blur(function(){
+         if(password.val() != ''){
+           if(password.val().length < 6){
+             $(#valid_password_message').text('Minimal password length 6 chare');
+             $('input[type=submit]').attr('disabled',true);
+           }else{
+             $('#valid_password_message').text('');
+             $('input[type=submit]').attr('disabled', false);
+           }
+         }else{
+           $('#valid_password_message').text('Input password!');
+         }
+       });
+     });
+   </script> 
+  </head>
   <body>
 
     <div id="header">
